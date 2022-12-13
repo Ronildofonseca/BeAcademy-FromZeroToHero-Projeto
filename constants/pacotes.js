61ef9310db2d5087226bd4b095282515a8c8ef55
Ronildo Fonseca <jrff.ronyjr@gmail.com>
2022-12-13 11:03:13 -0300

    alterações js

---
+++ constants/pacotes.js
@@ -0,0 +1,56 @@
+export const SEMANAL = {
+    titles: [
+      { title: "Semanal" },
+
+    ],
+    message: [
+      {
+        text: ` Nosso pacote semanal é composto por 10 refeições, contendo 5 almoços e 5 jantas, que
+        tornará sua dieta prazerosa, com efeito de saciedade e com excelência no sabor.<br />
+        Garantindo uma vida muito mais saudável, com uma nutrição acompanhada por um
+        especialista e um baixo teor de carboidratos!<br />
+        <strong>
+        Não deixe para segunda, a dieta que Low Carb te oferece para todos os dias!
+        </strong>`
+
+      },
+    ],
+  };
+
+  export const QUINZENAL = {
+    titles: [
+      { title: "Quinzenal" },
+
+    ],
+    message: [
+      {
+        text: ` Nosso pacote quinzenal é composto por 20 refeições, contendo 10 almoços e 10 jantas, que
+        tornará sua dieta prazerosa, com efeito de saciedade e com excelência no sabor.<br />
+        Garantindo uma vida muito mais saudável, com uma nutrição acompanhada por um
+        especialista e um baixo teor de carboidratos!<br />
+        <strong>
+        Não deixe para segunda, a dieta que Low Carb te oferece para todos os dias!
+        </strong>`
+
+      },
+    ],
+  };
+
+  export const MENSAL = {
+    titles: [
+      { title: "Semanal" },
+
+    ],
+    message: [
+      {
+        text: ` Nosso pacote Mensal é composto por 40 refeições, contendo 20 almoços e 20 jantas, que
+        tornará sua dieta prazerosa, com efeito de saciedade e com excelência no sabor.<br />
+        Garantindo uma vida muito mais saudável, com uma nutrição acompanhada por um
+        especialista e um baixo teor de carboidratos!<br />
+        <strong>
+        Não deixe para segunda, a dieta que Low Carb te oferece para todos os dias!
+        </strong>`
+
+      },
+    ],
+  };
